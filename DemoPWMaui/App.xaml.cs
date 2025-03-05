445f5c0a67935190260df8c387f1261aa9fb3956
chiara fusaroli <chiara.fusaroli@ispascalcomandini.it>
2025-03-05 18:12:04 +0100

    Aggiungere i file di progetto.

---
+++ DemoPWMaui/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DemoPWMaui.Views;
+
+namespace DemoPWMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // NAVIGAZIONE: impostata la schermata iniziale dell'app,
+            // ovvero la 'root page' alla base dello stack di navigazione.  
+
+            MainPage = new NavigationPage(new ListPage());
+        }
+    }
+}
